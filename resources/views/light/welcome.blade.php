edef05f4c147c599a6cabec796263e8254dcb370
mahangm <mahangm@gmail.com>
2019-10-26 23:12:32 +0330

    Improved welcome page

--- resources/views/light/welcome.blade.php
+++ resources/views/light/welcome.blade.php
@@ -2,7 +2,12 @@
 
 @section('content')
 <div class="container">
-    <h1 class="mt-3">Welcome!</h1>
-    <p>{!! __('Install your community by going to <a href=":url">/install</a> route!', ['url' => route('install')]) !!}</p>
+    <div class="jumbotron mt-3">
+        <h1 class="display-4">{{ __('Welcome!') }}</h1>
+        <p class="lead">{{ __('Thank you for choosing Converse as your community manager. Although Converse is a fairly simple system, you will not be disappointed by its capabilites. Let\'s get started by installing Converse via the installation wizard.') }}</p>
+        <hr class="my-4">
+        <p>{!! __('Click on Install button down below to head to the installation wizard or simply goto <a href=":url">/install</a> route.', ['url' => route('install')]) !!}</p>
+        <a class="btn btn-primary btn-lg" href="{{ route('install') }}" role="button">Install</a>
+    </div>
 </div>
 @endsection
